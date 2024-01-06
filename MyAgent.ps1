094d535d27c9d92b40fae5d19ff982a514d4e646
Joseph Mitro <jozef.mitro.sk@gmail.com>
2024-01-06 17:13:22 +0100

    Refactor authenticated get calls

--- MyAgent.ps1
+++ MyAgent.ps1
@@ -1,46 +1,4 @@
 param($Agent, $Mock = $false)
 
-if (!$Agent) {
-    $settings = Get-Content -Path 'settings.json' -Raw | ConvertFrom-Json
-    $Agent = $settings.CurrentAgent
-
-    if (!$Agent) {
-        Write-Host 'No agent specified and no agent found in settings.json'
-
-        exit
-    }
-}
-
-if ($Mock) {
-    $api = 'https://stoplight.io/mocks/spacetraders/spacetraders/96627693'
-}
-else {
-    $api = 'https://api.spacetraders.io/v2'
-}
-
-if (!(Test-Path -Path "Agents\$Agent\Register.token")) {
-    Write-Host "No register token found for agent: $Agent"
-
-    exit
-}
-
-$secureToken = Get-Content -Path "Agents\$Agent\Register.token" | ConvertTo-SecureString
-
-if (!$secureToken) {
-    Write-Host "Failed to load the token for agent: $Agent"
-
-    exit
-}
-
-$response = Invoke-RestMethod -Authentication Bearer -Token $secureToken -Uri "$api/my/agent" -Method Get -SkipHttpErrorCheck -StatusCodeVariable status
-
-if ($status -eq 200) {
-    $response | ConvertTo-Json -Depth 100 | Out-File -FilePath "Agents\$Agent\MyAgent.json"
-    $statusColor = 'Green'
-}
-else {
-    $statusColor = 'Red'
-}
-
-Write-Host "Status code: $status" -ForegroundColor $statusColor
-ConvertTo-Json $response -Depth 100
+$path = Join-Path -Path $PSScriptRoot -ChildPath 'AuthenticatedGet.ps1'
+& $path -Agent $Agent -Route 'my/agent' -Mock $Mock
