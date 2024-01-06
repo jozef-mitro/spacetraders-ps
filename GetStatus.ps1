4cbc81a5ff60cf9ecc67dcd235dad872ac850a83
Joseph Mitro <jozef.mitro.sk@gmail.com>
2024-01-06 12:17:48 +0100

    Catch http errors

--- GetStatus.ps1
+++ GetStatus.ps1
@@ -1,2 +1,3 @@
-$response = Invoke-RestMethod -Uri 'https://api.spacetraders.io/v2/' -Method Get
-$response
+$response = Invoke-RestMethod -Uri 'https://api.spacetraders.io/v3' -Method Get -SkipHttpErrorCheck -StatusCodeVariable status
+Write-Host "Status code: $status"
+ConvertTo-Json $response -Depth 100
